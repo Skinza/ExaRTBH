509b676942d632581fc7e124cd74f2fab33fb643
Bas van Elburg <basv@nelburg.com>
2016-02-26 16:08:28 +0100

    better file handling/removal detection

--- ExaRTBH.php
+++ ExaRTBH.php
@@ -7,6 +7,7 @@
 	// config
 	$config = array( 
 		'running' => true,
+		'debug' => true, // warning, this will produce debug data to stdout!
 		'blackholeipfile' => 'blackholeips',
 		'filecheckinterval' => 5,
 		'blackhole_bgp_community' => "65001:666",
@@ -19,20 +20,40 @@
 
 	logstr("Booting....");
 	while($config['running']) {
-		// does the file exist?
-		if(!is_file($config['blackholeipfile'])) {
-                	if(!is_dir($config['blackholeipfile'])) {
-				// file does not exist, create one and write known blackhole entries to file!
-                        	logstr2("WARNING","blackhole ip file not found, (re)creating file [" . $config['blackholeipfile'] . "]");
-				generateBlackholeFile();
-                	}
-        	}
-		// load entries from blackhole file
-		$newBlackholeEntries = loadBlackholesFromFile($config['blackholeipfile']);
+		// does the file exist? no? write memory to file, else parse 
+		if(!file_exists($config['blackholeipfile'])) {
+			// file does not exist, create one and write known blackhole entries to file!
+                        logstr2("WARNING","blackhole ip file not found, (re)creating file [" . $config['blackholeipfile'] . "]");
+			generateBlackholeFile();
+        	} else {
+			// load entries from blackhole file
+			$newBlackholeEntries = loadBlackholesFromFile($config['blackholeipfile']);
 
-		// compare current blackholes with new ones
-		
-	        
+			// loop trough new blackhole entries to see what action is needed
+			foreach($newBlackholeEntries as $newBlackhole) {
+				// loop trough new set of prefixes
+				$prefixFound = false;
+				foreach($blackholes as $key => $blackhole) {
+					if($blackhole == $newBlackhole) {
+						// blackhole is allready active, no action required
+						unset($blackholes[$key]); // remove from current list, will be replaced when new list is processed
+						$prefixFound = true;
+						break;
+					}
+				}
+				if(!$prefixFound) {
+					echo $newBlackhole->announceString() . "\n";
+				}
+			} 
+			
+			//process prefixes in current list to withdrawel these routes
+			foreach($blackholes as $blackhole) {
+				echo $blackhole->withdrawString()."\n";
+			}
+
+			//processing complete, replace old list with the new one
+			$blackholes = $newBlackholeEntries;
+		}
 
 		// generate hash of file
         	$config['blackholiphash'] = md5_file($config['blackholeipfile']);
@@ -40,7 +61,8 @@
 		
 		// wait for change of file
 		logstr("waiting...");
-		while(md5_file($config['blackholeipfile']) == $config['blackholiphash']) {
+		
+		while(@md5_file($config['blackholeipfile']) == $config['blackholiphash']) {
 			sleep($config['filecheckinterval']);
 		}  
 			
@@ -53,7 +75,10 @@
 		logstr2("INFO",$str_msg);
 	}
 	function logstr2($loglevel,$str_msg) {
-		echo "[".date("Y-m-d h:i:s")."] [".$loglevel."] ".$str_msg . "\n";
+		global $config;
+		if($config['debug']) {
+			echo "[".date("Y-m-d H:i:s")."] [".$loglevel."] ".$str_msg . "\n";
+		}
 	}
 
 	// load blackholes from file and return them als an array of BlackHoleEntries
@@ -64,7 +89,7 @@
 		foreach(file($file) as $row) {
 			$row = trim($row); // trim the line
 			// if it is not a comment 
-			if(substr($row,0,1) != "#") 
+			if(substr($row,0,1) != "#") { 
 				// parse the row, it is 1 of the following formats:
 				//  network/prefixlen (127.0.0.1/32) (only /32 supported right now)
 				// more formats will be supported in the future 
@@ -72,6 +97,7 @@
 					$newBlackholes[] = new BlackholeEntry(trim($row),$config['blackhole_bgp_community'],$config['blackhole_nexthop']);
 				} else {
 					logstr2("WARNING", "Ignoring entry (unexpected format) [".$row."]");
+				}
 			}
 		}
 		return $newBlackholes;
@@ -96,6 +122,7 @@
 		public $prefix;
 		public $bgp_community;
 		public $nexthop;
+		public $localpref = 4000;
 		function __construct($_prefix,$_bgp_community,$_nexthop) {
 			$this->prefix = $_prefix;
 			$this->bgp_community = $_bgp_community;
@@ -106,5 +133,9 @@
 		function toBlackholeFileString() {
 			return $this->prefix . "\n";
 		}
+		// tbd add local pref to data class
+		// tbd better checks
+		function announceString() { return "announce route $this->prefix next-hop $this->nexthop local-preference $this->localpref community [$this->bgp_community];"; }
+		function withdrawString() { return "withdraw route $this->prefix next-hop $this->nexthop local-preference $this->localpref community [$this->bgp_community];"; }
 	}
 ?>

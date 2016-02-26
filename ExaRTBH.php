34935e69fb3627a3a192ff3fde5b43f71d1700f5
Bas van Elburg <basv@nelburg.com>
2016-02-26 13:49:55 +0100

    added generate blackhole file

--- ExaRTBH.php
+++ ExaRTBH.php
@@ -11,7 +11,10 @@
 		'filecheckinterval' => 5
 	);
 
+	// "internal" list of active blackhole routes
+	$blackholes = array();
 	
+
 	logstr("Booting....");
 	logstr("Loading routes from file ". $config['blackholeipfile']);
 	
@@ -20,6 +23,12 @@
 			logstr2("WARNING","blackhole ip file not found, creating empty file");
 		}
 	}
+
+// some debug testing black hole entries
+$blackholes[] = new BlackHoleEntry("a","b","c");
+$blackholes[] = new BlackHoleEntry("d","e","f");
+$blackholes[] = new BlackHoleEntry("g","h","i");
+
 
 	// should announce all loaded prefixes
 	
@@ -43,5 +52,36 @@
 	}
 	function logstr2($loglevel,$str_msg) {
 		echo "[".date("Y-m-d h:i:s")."] [".$loglevel."] ".$str_msg . "\n";
+	}
+
+	// generateBlackhole file content en write to file
+	function generateBlackholeFile()
+	{
+		global $blackholes,$config;
+		$string2write = "";
+		foreach($blackholes as $bhEntry) {
+			$string2write .= $bhEntry->toBlackholeFileString();
+		}
+		file_put_contents($config['blackholeipfile'],$string2write);
+		logstr("blackholes written to new file");		
+	}
+
+	
+
+	/* data classes */
+	class BlackholeEntry {
+		public $prefix;
+		public $bgp_community;
+		public $nexthop;
+		function __construct($_prefix,$_bgp_community,$_nexthop) {
+			$this->prefix = $_prefix;
+			$this->bgp_community = $_bgp_community;
+			$this->nexthop = $_nexthop;	
+		}
+
+		// returns the string to add to the blackhole file (including trailing \n)
+		function toBlackholeFileString() {
+			return $this->prefix . "\n";
+		}
 	}
 ?>
